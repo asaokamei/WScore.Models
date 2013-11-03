44b7ed536046cc5be7959ec56ea71526d934ecc5
Asao <kamei@workspot.jp>
2013-11-03 21:33:31 +0900

    create SetTimeUpdatedAt and SetTimeTrait.

---
+++ src/WScore/DbGateway/Filter/SetTimeTrait.php
@@ -0,0 +1,40 @@
+<?php
+namespace WScore\DbGateway\Filter;
+
+use WScore\DbGateway\QueryInterface;
+
+trait SetTimeTrait
+{
+    protected $column_name;
+
+    protected $current_time;
+
+    /**
+     * sets name for created_at column.
+     *
+     * @param string $name
+     */
+    public function setColumnName( $name )
+    {
+        $this->column_name = $name;
+    }
+
+    public function setCurrentTime( $now )
+    {
+        $this->current_time = $now;
+    }
+
+    /**
+     * @param QueryInterface $query
+     * @param string         $column
+     */
+    protected function setTime( $query, $column )
+    {
+        if( !$query->getValue( $column ) ) return;
+        if( !$this->current_time ) {
+            $this->current_time = new \DateTime('now');
+            $this->current_time = $this->current_time->format( 'Y-m-d H:i:s' );
+        }
+        $query->setValue( $column, $this->current_time );
+    }
+}
--- src/WScore/DbGateway/Filter/SetTimeUpdatedAt.php
+++ src/WScore/DbGateway/Filter/SetTimeUpdatedAt.php
@@ -3,14 +3,14 @@ namespace WScore\DbGateway\Filter;
 
 use WScore\DbGateway\QueryInterface;
 
-class SetTimeCreatedAt extends FilterAbstract
+class SetTimeUpdatedAt extends FilterAbstract
 {
     use SetTimeTrait;
-    
+
     /**
      * @param QueryInterface $query
      */
-    protected function onInsert( $query )
+    protected function onUpdate( $query )
     {
         $this->setTime( $query, $this->column_name );
     }
