0e74cf1410b8639c3d69f7a2885045c323843027
Asao <kamei@workspot.jp>
2014-05-07 19:32:22 +0900

    add test suites to run all tests.

---
+++ tests/unittests/all-tests.php
@@ -0,0 +1,12 @@
+<?php
+
+class DbGateway_tests
+{
+    public static function suite()
+    {
+        $suite = new \PHPUnit_Framework_TestSuite( 'DbGateway Test Suites' );
+        $suite->addTestFile( __DIR__.'/ConstructTest/Dao_UnitTest.php' );
+        $suite->addTestFile( __DIR__.'/ConverterTest/Converter_Test.php' );
+        return $suite;
+    }
+}
