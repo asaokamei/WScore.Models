1e194e93a48c25aea5e78b85a4f16067a40e99e0
Asao <kamei@workspot.jp>
2014-05-14 09:59:56 +0900

    rename test to Dao_BasicTest and add it to the test suite.

--- tests/all-tests.php
+++ tests/all-tests.php
@@ -7,6 +7,7 @@ class DbGateway_tests
         $suite = new \PHPUnit_Framework_TestSuite( 'DbGateway Test Suites' );
         $suite->addTestFile( __DIR__.'/unitTests/ConverterTest/Converter_Test.php' );
         $suite->addTestFile( __DIR__.'/unitTests/EnumTest/Enum_Test.php' );
+        $suite->addTestFile( __DIR__.'/unitTests/DaoTest/Dao_BasicTest.php' );
         $suite->addTestFile( __DIR__.'/unitTests/DaoArrayTest/DaoArray_UnitTest.php' );
         $suite->addTestFile( __DIR__.'/FunctionalTests/DaoFunctionTest/Dao_BasicTest.php' );
         return $suite;
